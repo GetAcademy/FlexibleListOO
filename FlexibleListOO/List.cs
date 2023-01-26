e278cc862fbaa65b94dfa122f8a85d4949e06962
GET Academy <terje@getacademy.no>
2023-01-26 12:52:01 +0100

    Initial commit

---
+++ FlexibleListOO/List.cs
@@ -0,0 +1,40 @@
+﻿namespace FlexibleListOO
+{
+    internal class List
+    {
+        private int[] _numbers;
+        private int _count;
+        public List()
+        {
+            _numbers = new int[4];
+        }
+
+        public void Add(int number)
+        {
+            var index = _count;
+            if (index >= _numbers.Length)
+            {
+                Resize();
+            }
+            _numbers[index] = number;
+            _count++;
+        }
+
+        private void Resize()
+        {
+            var newNumbers = new int[_numbers.Length * 2];
+            Array.Copy(_numbers, newNumbers, _numbers.Length);
+            _numbers = newNumbers;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Count={_count}");
+            foreach (var n in _numbers)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ FlexibleListOO/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+
+    pause til 13:00
+
+ - lite fleksible arrayer i C#
+ - => objektorientering
+ - List<>
+ - generics
+ */
+
+using FlexibleListOO;
+
+var list = new List();
+while (true)
+{
+    var numberStr = Console.ReadLine();
+    var number = Convert.ToInt32(numberStr);
+    list.Add(number);
+    list.Show();
+}
+
+//var numbers = new int[4];
+//var count = 0;
+//while (true)
+//{
+//    var numberStr = Console.ReadLine();
+//    var number = Convert.ToInt32(numberStr);
+//    var index = count;
+//    if (index >= numbers.Length)
+//    {
+//        var newNumbers = new int[numbers.Length*2];   
+//        Array.Copy(numbers, newNumbers, numbers.Length);
+//        numbers = newNumbers;
+//    }
+//    numbers[index] = number;
+//    count++;
+
+//    Console.WriteLine($"Count={count}");
+//    foreach (var n in numbers)
+//    {
+//        Console.Write(n + " ");
+//    }
+//    Console.WriteLine();
+//}
